0ea490274c898ed09afa992994afc57bc03dbe5f
lineash <olol5454@naver.com>
2021-11-29 16:51:01 +0900

    Initial Commit

---
+++ coloring/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  coloring
+//
+//  Created by line.ash on 2021/11/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
